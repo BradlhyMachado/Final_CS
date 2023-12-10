cbbfbbcd2e5f015ae487310b9dc5292f5cb0ed56
Bradlhy Luis Machado Medina <bmachadom@ulasalle.edu.pe>
2023-12-09 22:00:19 -0500

    Final version

--- app.py
+++ app.py
@@ -21,5 +21,29 @@ def analizar_sentimiento():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+
+@app.route('/promedio_sentimientos', methods=['POST'])
+def analizar_sentimiento():
+    try:
+        data = request.get_json()
+        comentarios = data['comentarios']
+
+        total_polaridad = 0
+        total_comentarios = len(comentarios)
+
+        for comentario in comentarios:
+            resultado = classifier(comentario)[0]
+            
+            total_polaridad += int(resultado['label'][0])
+
+        if total_comentarios > 0:
+            promedio_polaridad = round(total_polaridad / total_comentarios, 2)
+        else:
+            promedio_polaridad = 0
+
+        return jsonify({'promedio_polaridad': promedio_polaridad})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
